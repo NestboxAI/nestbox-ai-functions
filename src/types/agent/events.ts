b4367e3534952c3c599b3cbd2ec2fb87e98759cc
Roy Ganor <ganoro@gmail.com>
2025-10-31 11:17:19 -0400

    fix: emit* should return a promise

--- src/types/agent/events.ts
+++ src/types/agent/events.ts
@@ -8,23 +8,23 @@ export interface AgentEvents {
      * Emits an event when a query is created.
      * @param event - An object containing the data related to the created query.
      */
-    emitQueryCreated(event: AgentEventPayload): void;
+    emitQueryCreated(event: AgentEventPayload): Promise<void>;
 
     /**
      * Emits an event when a query is successfully completed.
      * @param event - An object containing the data related to the completed query.
      */
-    emitQueryCompleted(event: AgentEventPayload): void;
+    emitQueryCompleted(event: AgentEventPayload): Promise<void>;
 
     /**
      * Emits an event when a query fails.
      * @param event - An object containing the data related to the failed query.
      */
-    emitQueryFailed(event: AgentEventPayload): void;
+    emitQueryFailed(event: AgentEventPayload): Promise<void>;
 
     /**
      * Emits an event when a generic event is created.
      * @param event - An object containing the data related to the created event.
      */
-    emitEventCreated(event: AgentEventPayload): void;
+    emitEventCreated(event: AgentEventPayload): Promise<void>;
 }
